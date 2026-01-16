ccb2e3a3d417a3b489261d99e236c22b4cdcd750
Omkar Ranawade <omkar.ranawade98@gmail.com>
2026-01-16 12:38:38 -0500

    Initial Commit

---
+++ ChargeITTests/ChargeITTests.swift
@@ -0,0 +1,17 @@
+//
+//  ChargeITTests.swift
+//  ChargeITTests
+//
+//  Created by Omkar  on 1/16/26.
+//
+
+import Testing
+@testable import ChargeIT
+
+struct ChargeITTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ ChargeITUITests/ChargeITUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  ChargeITUITestsLaunchTests.swift
+//  ChargeITUITests
+//
+//  Created by Omkar  on 1/16/26.
+//
+
+import XCTest
+
+final class ChargeITUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
